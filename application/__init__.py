da522708c229baaba4e6f86be972a875b926978d
Esukaa <mourad.benyahia@epitech.eu>
2024-02-27 10:10:15 +0000

    changing MongoUri

--- application/__init__.py
+++ application/__init__.py
@@ -13,8 +13,7 @@ def createapp():
     db = os.getenv("MONGO_DB")
     app = Flask(__name__)
 
-    app.config["MONGO_URI"] = "mongodb+srv://root:toor@recycle.uqnlrsp.mongodb.net/todos?retryWrites=true&w=majority&appName=Recycle"
-
+    app.config["MONGO_URI"] = f"mongodb+srv://{user}:{password}@{host}/{db}?retryWrites=true&w=majority&appName=Recycle"
     mongo.init_app(app)
 
     app.register_blueprint(main)
